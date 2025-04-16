c541d7b46f3ee1fed85bc10a05ffd733cba349d0
DanielContrerasCh <danicontreras0409@gmail.com>
2025-04-15 22:26:14 -0600

    Añadir eslint para seguir estandares de codigo

--- harvester-app/src/main.js
+++ harvester-app/src/main.js
@@ -1,13 +1,14 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('node:path');
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+// Comprobar si la aplicación se está ejecutando en modo de instalación de Squirrel
+// y salir si es así. Esto es necesario para evitar que la aplicación se inicie
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
 const createWindow = () => {
-  // Create the browser window.
+  // Crear la ventana del navegador.
   const mainWindow = new BrowserWindow({
     width: 1920,
     height: 1080,
@@ -16,21 +17,19 @@ const createWindow = () => {
     },
   });
 
-  // and load the index.html of the app.
+  // Cargar el archivo HTML de inicio de sesión.
   mainWindow.loadFile(path.join(__dirname, './frontend/vistas/inicioSesion.html'));
 
-  // Open the DevTools.
+  // Abrir las herramientas de desarrollo.
   // mainWindow.webContents.openDevTools();
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+// Este método se llamará cuando Electron haya terminado de inicializar
+// y esté listo para crear ventanas del navegador.
 app.whenReady().then(() => {
   createWindow();
 
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
+  // Cerrar la ventana principal cuando se cierra la última ventana.
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
@@ -38,14 +37,10 @@ app.whenReady().then(() => {
   });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// Cerrar la aplicación cuando todas las ventanas estén cerradas
+// Esto es necesario para evitar que la aplicación se cierre en macOS
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
