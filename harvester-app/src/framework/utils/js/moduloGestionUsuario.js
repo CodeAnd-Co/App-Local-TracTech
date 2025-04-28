78f5875743adf6de8520df47c93d3bc608eb9ef1
Rommel <rommtoledo@gmail.com>
2025-04-27 23:48:36 -0600

    Añadir la funcionalidad de consulta de usuarios a la vista

--- harvester-app/src/framework/utils/js/moduloGestionUsuario.js
+++ harvester-app/src/framework/utils/js/moduloGestionUsuario.js
@@ -1,9 +1,48 @@
-// Función para inicializar el módulo de análisis
-function inicializarModuloGestionUsuarios() {
+async function inicializarModuloGestionUsuarios() {
     console.log("Módulo de Gestión de Usuarios inicializado");
-  
-    // Asegurar que el localStorage esté actualizado
     localStorage.setItem('seccion-activa', 'gestionUsuarios');
+
+    try {
+        const { obtenerUsuarios } = require('../../backend/casosUso/usuarios/consultarUsuarios.js');
+        const usuarios = await obtenerUsuarios();
+        mostrarUsuarios(usuarios?.obtenerUsuarios() ?? []);
+    } catch (error) {
+        console.error("Error al obtener usuarios:", error);
+    }
+}
+
+function mostrarUsuarios(usuarios) {
+    const listaUsuarios = document.getElementById('lista-usuarios');
+    if (!listaUsuarios) {
+        console.error("No se encontró el elemento de la lista de usuarios en el DOM.");
+        return;
+    }
+
+    listaUsuarios.innerHTML = '';
+
+    if (usuarios.length === 0) {
+        listaUsuarios.innerHTML = '<li>No hay usuarios disponibles.</li>';
+        return;
+    }
+
+    const fragmento = document.createDocumentFragment();
+    for (const { nombre } of usuarios) {
+        const div = document.createElement('div');
+        div.className = 'frame-usuario';
+        div.innerHTML = `
+            <div class="nombre-usuario">
+                <div class="texto-usuario">${nombre}</div>
+            </div>
+            <div class="editar">
+                <img class="editar-icono" src="../utils/iconos/Editar2.svg" />
+            </div>
+            <div class="eliminar">
+                <img class="eliminar-icono" src="../utils/iconos/BasuraBlanca.svg" />
+            </div>
+        `;
+        fragmento.appendChild(div);
+    }
+    listaUsuarios.appendChild(fragmento);
 }
 
 window.inicializarModuloGestionUsuarios = inicializarModuloGestionUsuarios;
