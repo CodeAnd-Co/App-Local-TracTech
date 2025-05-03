4c80a3e78cd34bcca39b7dede031644b2be8afe8
DanielContrerasCh <danicontreras0409@gmail.com>
2025-05-02 19:20:25 -0600

    Agregar comentarios

--- harvester-app/src/backend/casosUso/sesion/iniciarSesion.js
+++ harvester-app/src/backend/casosUso/sesion/iniciarSesion.js
@@ -14,6 +14,17 @@ function validarCorreo(correo) {
   return regex.test(correo);
 }
 
+/**
+ * Sanitiza los datos de entrada del usuario, como correo y contraseña.
+ *
+ * Utiliza `validator.normalizeEmail` para normalizar el correo electrónico y 
+ * `validator.escape` para proteger contra inyecciones en la contraseña escapando caracteres especiales.
+ *
+ * @function sanitizarEntrada
+ * @param {string} correo - Correo electrónico ingresado por el usuario.
+ * @param {string} contrasena - Contraseña ingresada por el usuario.
+ * @returns {{correoSanitizado: string, contrasenaSanitizada: string}} Objeto con el correo y la contraseña sanitizados.
+ */
 function sanitizarEntrada(correo, contrasena) {
   const correoSanitizado = validator.normalizeEmail(correo);
   const contrasenaSanitizada = validator.escape(contrasena); // Escapa caracteres peligrosos
@@ -34,8 +45,9 @@ async function iniciarSesion(correo, contrasena) {
     return { ok: false, mensaje: 'Correo inválido' };
   }
 
+  // Sanitizar la entrada del correo y la contraseña
   const { correoSanitizado, contrasenaSanitizada } = sanitizarEntrada(correo, contrasena);
-  
+
   try {
 
     const respuesta = await iniciarSesionAPI(correoSanitizado, contrasenaSanitizada);
