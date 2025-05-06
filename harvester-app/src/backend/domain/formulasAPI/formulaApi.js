efe9fccada6149fe4b4ca5e28309be46ac049f5a
Juan Pablo <a01705408@tec.mx>
2025-05-06 10:47:17 -0600

    Adición de funcionalidad

---
+++ harvester-app/src/backend/domain/formulasAPI/formulaApi.js
@@ -0,0 +1,18 @@
+// RF67 Crear Fórmula - https://codeandco-wiki.netlify.app/docs/proyectos/tractores/documentacion/requisitos/RF67 
+async function guardarFormula(nombre, formula){
+    console.log("Nombre de la fórmula (API):", nombre, "Fórmula:", formula);
+    const respuesta = await fetch("http://localhost:3000/formulas/guardarFormula", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({nombre, formula}),
+    });
+
+    const datos = await respuesta.json();
+    return { ok: respuesta.ok, ...datos };
+}
+
+module.exports = {
+    guardarFormula,
+};
