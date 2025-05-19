1a0e6acd568a873cb4ed11cf44f5ef9f3df848b6
July230 <A01352823@tec.mx>
2025-05-19 11:46:33 -0600

    Usar try catch en funcion

--- harvester-app/src/backend/casosUso/excel/seleccionaDatosAComparar.js
+++ harvester-app/src/backend/casosUso/excel/seleccionaDatosAComparar.js
@@ -9,28 +9,33 @@
  * @returns {Object} JSON nuevo con los datos y columnas seleccionadas 
  */
 function seleccionaDatosAComparar(datosExcel, seleccion) {
-    const nuevoJSON = { hojas: {} };
-
-    Object.entries(seleccion).forEach(([nombreTractor, columnas]) => {
-        // Obtener los datos completos del tractor desde el JSON original
-        const datosTractor = datosExcel.hojas[nombreTractor];
-
-        if (!datosTractor || !Array.isArray(columnas) || columnas.length === 0) return;
-
-        // Para cada fila del tractor, creamos una nueva fila con solo las columnas seleccionadas
-        nuevoJSON.hojas[nombreTractor] = datosTractor.map(fila =>
-            columnas.reduce((nuevoFila, columna) => {
-                // Agregamos la columna seleccionada a la nueva fila
-                nuevoFila[columna] = fila[columna];
-                return nuevoFila;
-            }, {})
-        );
-    });
-
-    // Guardar el nuevo JSON en localStorage
-    localStorage.setItem('datosFiltradosExcel', JSON.stringify(nuevoJSON));
-
-    return nuevoJSON;
+    try {
+        const nuevoJSON = { hojas: {} };
+    
+        Object.entries(seleccion).forEach(([nombreTractor, columnas]) => {
+            // Obtener los datos completos del tractor desde el JSON original
+            const datosTractor = datosExcel.hojas[nombreTractor];
+    
+            if (!datosTractor || !Array.isArray(columnas) || columnas.length === 0) return;
+    
+            // Para cada fila del tractor, creamos una nueva fila con solo las columnas seleccionadas
+            nuevoJSON.hojas[nombreTractor] = datosTractor.map(fila =>
+                columnas.reduce((nuevoFila, columna) => {
+                    // Agregamos la columna seleccionada a la nueva fila
+                    nuevoFila[columna] = fila[columna];
+                    return nuevoFila;
+                }, {})
+            );
+        });
+    
+        // Guardar el nuevo JSON en localStorage
+        localStorage.setItem('datosFiltradosExcel', JSON.stringify(nuevoJSON));
+    
+        return nuevoJSON;
+    } catch (error) {
+        console.error('Error verificando archivo:', error);
+        throw new Error('Error al comparar datos');
+    }
 }
 
 module.exports = {
