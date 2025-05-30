5832b6731482f9fa4361d6397232b6ebde18430f
Diego <a01705506@tec.mx>
2025-05-29 20:45:45 -0600

    [Fix] - Integración con develop

--- harvester-app/eslint.config.mjs
+++ harvester-app/eslint.config.mjs
@@ -16,6 +16,7 @@ export default [
       
       globals: {
         rutaBase: 'readonly',
+        permisos: 'readonly',
       },
     },
     rules: {
--- harvester-app/src/framework/utils/scripts/iniciarSesion.js
+++ harvester-app/src/framework/utils/scripts/iniciarSesion.js
@@ -38,7 +38,8 @@ async function manejarInicioSesion() {
       const usuario = resultado.usuario;
       localStorage.setItem('nombreUsuario', usuario);
       const rutaInicio = `${rutaBase}src/framework/vistas/paginas/inicio/inicio.ejs`;
-      try {          const vista = await ipcRenderer.invoke('precargar-ejs', rutaInicio,{Seccion: 'Inicio', Icono: 'Casa', permisos: listaPermisos});
+      try {
+          const vista = await ipcRenderer.invoke('precargar-ejs', rutaInicio,{Seccion: 'Inicio', Icono: 'Casa', permisos: listaPermisos});
           window.location.href = vista;
       } catch (err) {
           console.error('Error al cargar vista:', err);
