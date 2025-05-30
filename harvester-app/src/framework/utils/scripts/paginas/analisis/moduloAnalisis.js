b04d9f210520a9f616b6bcfc6602d2c7985d1cca
Juan Carlos Calderón <150750774+Johnccg@users.noreply.github.com>
2025-05-30 12:17:19 -0600

    Arreglar espaciado de elementos en pdf

--- harvester-app/src/framework/utils/scripts/paginas/analisis/moduloAnalisis.js
+++ harvester-app/src/framework/utils/scripts/paginas/analisis/moduloAnalisis.js
@@ -118,8 +118,8 @@ async function descargarPDF() {
       let tamanoFuente = 11.5;
       let estiloFuente = 'normal';
       let espaciado = 50;
-      if (elemento.classList.contains('preview-titulo')) { tamanoFuente = 18; estiloFuente = 'bold', espaciado = 75; }
-      if (elemento.classList.contains('preview-subtitulo')) { tamanoFuente = 15; estiloFuente = 'bold', espaciado = 55; }
+      if (elemento.classList.contains('preview-titulo')) { tamanoFuente = 18; estiloFuente = 'bold', espaciado = 55; }
+      if (elemento.classList.contains('preview-subtitulo')) { tamanoFuente = 15; estiloFuente = 'bold', espaciado = 45; }
 
       documentoPDF.setFontSize(tamanoFuente);
       documentoPDF.setFont(undefined, estiloFuente);
