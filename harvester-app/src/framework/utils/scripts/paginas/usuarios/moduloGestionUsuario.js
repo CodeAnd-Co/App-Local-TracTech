12adda41af5ce03e25b5ca2cbe5eb3451ced8f81
Pablo H <h.h.m.pablo@gmail.com>
2025-06-01 22:15:03 -0600

    Cierra el menu antes de recargar

--- harvester-app/src/framework/utils/scripts/paginas/usuarios/moduloGestionUsuario.js
+++ harvester-app/src/framework/utils/scripts/paginas/usuarios/moduloGestionUsuario.js
@@ -391,6 +391,8 @@ function mostrarUsuarios(usuarios) {
             if (respuesta) {
                 await eliminarUsuario(id); // Ahora puedes usar await aquí
                 setTimeout(() => {
+                    const columnaCrear = document.getElementById('columna-crear-modificar-usuario');
+                    columnaCrear.style.display = 'none';
                     inicializarModuloGestionUsuarios(); // Recargar la lista de usuarios
                 }, 500);
             }
