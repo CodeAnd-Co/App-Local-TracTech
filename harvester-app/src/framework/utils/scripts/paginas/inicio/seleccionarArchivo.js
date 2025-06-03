9a160d71d972c9df9edbe7ab59030a3bb7462aee
Juan Pablo <a01705408@tec.mx>
2025-06-02 19:40:47 -0600

    Correcciones de palabras

--- harvester-app/src/framework/utils/scripts/paginas/inicio/seleccionarArchivo.js
+++ harvester-app/src/framework/utils/scripts/paginas/inicio/seleccionarArchivo.js
@@ -75,7 +75,7 @@ function botonCargar() {
         const botonAnalisis = document.querySelector('.avanzar-analisis');
         const botonBorrar = document.getElementById('boton-borrar');
 
-        const elemtnoBotonCargar = document.querySelector('.texto-cargar');
+        const elementoBotonCargar = document.querySelector('.texto-cargar');
 
 
         if (!entradaArchivos || !elementoNombreArchivo) {
@@ -85,7 +85,7 @@ function botonCargar() {
         if (localStorage.getItem('nombreArchivoExcel')) {
             // Si ya hay un archivo seleccionado, lo mostramos
             elementoNombreArchivo.textContent = localStorage.getItem('nombreArchivoExcel');
-            elemtnoBotonCargar.textContent = "Cambiar Archivo"
+            elementoBotonCargar.textContent = "Cambiar Archivo"
             // Habilitar el botón de borrar
             botonBorrar.style.display = 'block';
             // Habilitar el botón de análisis
@@ -103,21 +103,21 @@ function botonCargar() {
 
         // Texto mientras se procesa
         elementoNombreArchivo.textContent = 'Verificando archivo...';
-        elemtnoBotonCargar.textContent = "Cargando Archivo"
+        elementoBotonCargar.textContent = "Cargando Archivo"
 
         try {
             const resultado = await leerExcel(archivo);
 
             if (resultado.exito) {
                 elementoNombreArchivo.textContent = archivo.name;
-                elemtnoBotonCargar.textContent = "Cambiar Archivo"
+                elementoBotonCargar.textContent = "Cambiar Archivo"
                 botonAnalisis.removeAttribute('disabled');
                 botonBorrar.style.display = 'block';
                 localStorage.setItem('nombreArchivoExcel', archivo.name);
                 entradaArchivos.forEach(input => input.value = '');
             } else {
                 elementoNombreArchivo.textContent = 'Sin archivo seleccionado';
-                elemtnoBotonCargar.textContent = "Cargar Archivo"
+                elementoBotonCargar.textContent = "Cargar Archivo"
                 botonBorrar.style.display = 'none';
                 botonAnalisis.setAttribute('disabled', 'true');
                 localStorage.removeItem('nombreArchivoExcel');
@@ -127,7 +127,7 @@ function botonCargar() {
             }
         } catch (error) {
             elementoNombreArchivo.textContent = 'Sin archivo seleccionado';
-            elemtnoBotonCargar.textContent = "Cargar Archivo"
+            elementoBotonCargar.textContent = "Cargar Archivo"
             botonAnalisis.setAttribute('disabled', 'true');
             localStorage.removeItem('nombreArchivoExcel');
             localStorage.removeItem('datosExcel');
