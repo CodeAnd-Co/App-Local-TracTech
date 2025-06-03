4db572bbcd4551830dcf51d91409ce066b0aa4a2
Juan Pablo <a01705408@tec.mx>
2025-06-03 14:13:36 -0600

    console.logs

--- harvester-app/src/backend/casosUso/formulas/modificarFormula.js
+++ harvester-app/src/backend/casosUso/formulas/modificarFormula.js
@@ -48,7 +48,6 @@ async function modificarFormulaCasoUso(id, nombre, formula, nombreOriginal) {
         return;
         
     }
-    console.log(`Modificando fórmula con ID: ${id}, Nombre: ${nombre}, Fórmula: ${formula}`);
     const respuesta = await mostrarAlertaConfirmacion(
         'Modificar fórmula', 
         `¿Estás seguro de que deseas modificar la fórmula "${nombreOriginal}"? Asegúrate de que sea una fórmula válida.`, 
@@ -67,7 +66,7 @@ async function modificarFormulaCasoUso(id, nombre, formula, nombreOriginal) {
                 await mostrarAlerta('Fórmula modificada', 'La fórmula ha sido modificada exitosamente.', 'success');
                 window.cargarModulo('formulas'); 
             }catch (error) {
-                mostrarAlerta('Error', `Se pudo modificar la fórmula. Pero sucedio un error inesperado: ${error}`, 'error');
+                mostrarAlerta('Error', `Sucedio un error inesperado: ${error}`, 'error');
                 return;
             }
 
