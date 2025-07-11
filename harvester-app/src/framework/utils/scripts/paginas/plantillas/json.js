fc9598424ed682443bbdf093db3469f54949dfb7
Diego <a01705506@tec.mx>
2025-07-10 21:08:16 -0600

    avance en  UI

---
+++ harvester-app/src/framework/utils/scripts/paginas/plantillas/json.js
@@ -0,0 +1,22 @@
+
+
+{
+    componentes : [
+        {
+            componente : "texto",
+            tipo : "titulo",
+            contenido : "Reporte trimestral de CASE 110 A",
+            alineamiento : "izquierda",
+        },
+        {
+            componente : "grafica",
+            nombre : "Litros de gasolina consumidos",
+            tipo : "linea",
+            tractor : "CASE 110 A (TEC)",
+            parametro : '',
+            filtro : 'Filtro encendido',
+            formula : 'Exceso de Gasolina'
+
+        }
+    ]
+}
